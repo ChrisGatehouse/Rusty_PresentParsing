f6b4db93b5b3e7b5f95c1b87d17cf8f9781d2c53
Chris Gatehouse <cgate2@pdx.edu>
2019-07-28 00:54:14 -0700

    commit of some initial code that can parse my csv data

--- rusty_present_parser/src/main.rs
+++ rusty_present_parser/src/main.rs
@@ -1,3 +1,61 @@
+use std::error::Error;
+use std::io;
+use std::process;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Present {
+		Application: String, //0
+		ProcessID: String, //1
+		SwapChainAddress: String, //2
+		Runtime: String, //3
+		SyncInterval: u64, //4
+		PresentFlags: Option<u64>, //5
+		AllowsTearing: String, //6
+		PresentMode: String, //7
+		#[serde(deserialize_with = "csv::invalid_option")]
+		Dropped: Option<u64>, //8
+		TimeInSeconds: f64, //9
+		MsBetweenPresents: Option<f64>, //10
+		MsBetweenDisplayChange: f64, //11
+		MsInPresentAPI: Option<f64>, //12
+		MsUntilRenderComplete: Option<f64>, //13
+		MsUntilDisplayed: Option<f64>, //14		
+}
+
+/*
+fn example() -> Result<(), Box<dyn Error>> {
+    //let mut rdr = csv::Reader::from_reader(io::stdin());
+	let mut rdr = csv::Reader::from_path("..\\fortnite.csv")?;
+    for result in rdr.deserialize() {
+        // Notice that we need to provide a type hint for automatic
+        // deserialization.
+        let present: Present = result?;
+		//let ms1 = result.MsBetweenPresents;
+        println!("{:?}", present);
+		//println!("{:?}", ms1);
+    }
+    Ok(())
+}
+*/
+
+fn example() -> Result<(), Box<Error>> {
+    //let mut rdr = csv::Reader::from_reader(io::stdin());
+	let mut rdr = csv::Reader::from_path("..\\fortnite.csv")?;
+	let mut _totalFrameTime = 0.0;
+	for result in rdr.deserialize() {        
+		let record: Present = result?;
+		_totalFrameTime += record.MsBetweenDisplayChange;
+        //println!("{:?}", record.MsBetweenDisplayChange);
+    }
+	println!("{:?}", _totalFrameTime.to_owned());
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+	if let Err(err) = example() {
+		println!("error running example: {}", err);
+        process::exit(1);
+	}
 }
