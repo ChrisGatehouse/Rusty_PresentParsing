cc69628a85696538622c9bcda2274850b7b66e2b
Chris Gatehouse <cgate2@pdx.edu>
2019-07-31 00:19:53 -0700

    add some more calculations, still just working math

--- rusty_present_parser/src/main.rs
+++ rusty_present_parser/src/main.rs
@@ -45,10 +45,14 @@ fn example() -> Result<(), Box<dyn Error>> {
 
 fn example() -> Result<(), Box<Error>> {
     //let mut rdr = csv::Reader::from_reader(io::stdin());
-	let mut rdr = csv::Reader::from_path("..\\data\\fortnite.csv")?;
+	//let mut rdr = csv::Reader::from_path("..\\data\\fortnite.csv")?;
+	let mut rdr = csv::Reader::from_path("..\\data\\ThreeKingdoms_battle-0.csv")?;
+	
 	let mut _totalFrameTime = 0.0;
 	let mut _averageFPS = 0.0;
+	let mut _median_FPS = 0.0;
 	let mut _one_percent_fps = 0.0;
+	let mut _point_one_percent_fps = 0.0;
 	
 	let mut _frameTimesVec = vec![];
 	for result in rdr.deserialize() {        
@@ -64,7 +68,17 @@ fn example() -> Result<(), Box<Error>> {
 		
 	//make this a function so that we can return percent low fps i.e. 0.1%, 1%, 3%, etc.
 	let mut _one_percent_size = libm::ceil(_frameTimesVec.len() as f64 * 0.01) as u64;
+	let mut _point_one_percent_size = libm::ceil(_frameTimesVec.len() as f64 * 0.001) as u64;
 	_one_percent_fps = libm::floor(1000.0 / _frameTimesVec[_frameTimesVec.len() - _one_percent_size as usize]);
+	_point_one_percent_fps = libm::floor(1000.0 / _frameTimesVec[_frameTimesVec.len() - _point_one_percent_size as usize]);
+	
+	if _frameTimesVec.len() % 2 == 0 {
+		//if the set is even median is normally the mean of the two middle numbers but we want to see true numbers
+		//that actually existed in the set so take the lowest of the two
+		_median_FPS = 1000.0 / _frameTimesVec[(_frameTimesVec.len() / 2) - 1];
+	} else {
+		_median_FPS = libm::floor(1000.0 / _frameTimesVec[_frameTimesVec.len() / 2]);
+	}
 	
 	_averageFPS = 1000.0 / (_totalFrameTime / _frameTimesVec.len() as f64);
 	
@@ -72,7 +86,8 @@ fn example() -> Result<(), Box<Error>> {
 	//_one_percent_fps = libm::floor(1000.0 / _frameTimesVec[_frameTimesVec.len() - libm::ceil(_frameTimesVec.len() * 0.01)])
 	
 	println!("Size of one percent data set: {:?}", _one_percent_size);
-	println!("One percent low FPS: {:?}", _one_percent_fps);
+	println!("1% percent low FPS: {:?}", _one_percent_fps);
+	println!("0.1% percent low FPS: {:?}", _point_one_percent_fps);
 	println!("Total frame time in ms: {:?}", _totalFrameTime.to_owned());
     println!("Size of data set: {:?}", _frameTimesVec.len());
 	
@@ -93,6 +108,7 @@ fn example() -> Result<(), Box<Error>> {
 	*/
 	
 	println!("Average FPS calculated: {:?}", _averageFPS);
+	println!("Median FPS calculated: {:?}", _median_FPS);
 	Ok(())
 }
 
